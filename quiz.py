9ba56d2ae69f0da26522f48adf9c9b920b674b43
alexander ridley <aridley19@VenerableBede.schools.sunderland.gov.uk>
2016-11-11 10:56:03 +0000

    quiz do at home

--- quiz.py
+++ quiz.py
@@ -1,8 +1,97 @@
 # Our quiz!
 
-def quiz():
+score = 0
 
-    ... make a quiz!
+def quiz():
+    global score
+
+    question1()
+    question2()
+    question3()
+    question4()
+
+def question1():
+    global score
+
+    print("Who is the strongest")
+    print("A. bulbasour")
+    print("B. charmander")
+    print("C. squirtle")
+    print("D. pidgey")
+    answer = input("Select an option: ")
+
+    if answer.upper() == "A":
+        score = score + 50
+    elif answer.upper() == "B":
+        score = score + 100
+    elif answer.upper() == "C":
+        score = score + 75
+    elif answer.upper() == "D":
+        score = score + 25
+
+    print(score)
+
+def question2():
+    global score
+
+    print("which pokemon did ash get at the start of his journey")
+    print("A. squirtle")
+    print("B. pikachu")
+    print("C. metapod")
+    answer = input("Select an option: ")
+
+    if answer.upper() == "A":
+        score = score + 0
+    elif answer.upper() =="B":
+        score = score + 100
+    elif answer.upper() == "C":
+        score = score + a
+        15
+
+    print(score)
+
+def question3():
+    global score
+
+    print("Whick pokemon created the universe")
+    print("A. arceus")
+    print("B. mew")
+    print("C. pikachu")
+    answer = input("Select an option: ")
+    
+    if answer.upper() == "A":
+       score = score + 100
+
+    elif answer.upper() == "B":
+         score == score + 0
+
+    elif answer.upper() == "C":
+         score == score + 0
+
+         
+    print(score)
+
+def question4():
+    global score
+
+    print("what were the 3 pokemon X and Y started pokemon")
+    print("A. Froakie, Chespin and Fennekin")
+    print("B. Charmander, Bulbasour and Squirtle")
+    print("C. Piplup, Turtwig, and Chimchar")
+    print("D. Totodile, Cyndaquil and Chikorita")
+    answer = input("Select an option: ")
+
+    if answer.upper() == "A":
+      score = score + 150
+
+    else:
+        score = score + 0
+        
+    print(score)
+       
+
+         
+        
 
 
 
